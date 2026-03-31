60d1c0ebaba13caa265c03b6fecddcedd76fd849
Anoop Pemmaraju <anuppemmaraju@outlook.com>
2026-03-31 22:44:56 +0530

    Code Refactor.

--- src/Components/DevStreak.jsx
+++ src/Components/DevStreak.jsx
@@ -1,15 +1,19 @@
 import { useState, useEffect } from "react";
 import moment from "moment";
 function DevStreak() {
-  const [isCompleted, setIsCompleted] = useState(false);
-  const [done, setDone] = useState("Mark Today as Complete?");
+  // const [isCompleted, setIsCompleted] = useState(false);
+  const [isCompleted, setIsCompleted] = useState(
+    localStorage.getItem("lastCompleted") === moment().format("MMMM Do YYYY"),
+  );
+  // const [done, setDone] = useState("Mark Today as Complete?");
   const [streak, setStreak] = useState(0);
+  const today = moment().format("MMMM Do YYYY");
+  // const isCompleted = localStorage.getItem("lastCompleted") === today;
 
-  let newStreak = 1;
-  let difference = null;
   const handleTodaysDate = () => {
+    let newStreak = 1;
+    let difference = null;
     const lastCompletedDate = localStorage.getItem("lastCompleted");
-    const today = moment().format("MMMM Do YYYY");
     const prevStreak = Number(localStorage.getItem("streakCount")) || 0;
     if (lastCompletedDate) {
       difference = moment().diff(
@@ -25,27 +29,25 @@ function DevStreak() {
     localStorage.setItem("lastCompleted", today);
     localStorage.setItem("streakCount", newStreak);
     setIsCompleted(true);
-    setDone("Done!");
+    // setDone("Done!");
     setStreak(newStreak);
   };
 
   useEffect(() => {
-    const lastCompletedDate = localStorage.getItem("lastCompleted");
-    const today = moment().format("MMMM Do YYYY");
     //We update the status here as well, so that even after page is refreshed data persists.
     const storedStreak = Number(localStorage.getItem("streakCount")) || 0;
     setStreak(storedStreak);
-    if (today === lastCompletedDate) {
-      setDone("Done!");
-      setIsCompleted(true);
-    }
+    // if (today === lastCompletedDate) {
+    //   // setDone("Done!");
+    // setIsCompleted(true);
+    // }
   }, []);
   return (
     <div>
       <h1>Dev Streak 🔥</h1>
       {isCompleted && <span>Date: {moment().format("MMMM Do YYYY")}</span>}
       <button onClick={handleTodaysDate} disabled={isCompleted}>
-        ✅ {done}
+        {isCompleted ? "Done ✅😊" : "Mark Today as Complete? 🔥"}
       </button>
       <p>Streak: {streak} days</p>
     </div>
