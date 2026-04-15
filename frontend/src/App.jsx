efb512f42c33121b81d8ca96cbd9e9c03a4a7592
Anoop Pemmaraju <anuppemmaraju@outlook.com>
2026-04-15 20:58:27 +0530

    Front End Loading Added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,21 +2,30 @@ import "./App.css";
 import { useEffect, useState } from "react";
 function App() {
   const [message, setMessage] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  async function fetchMessage() {
+    try {
+      setLoading(true);
+      const response = await fetch("http://localhost:3000/api/message");
+      const messageData = await response.json();
+      setMessage(messageData.message);
+      setLoading(false);
+    } catch (err) {
+      console.error("Error Occured", err);
+      setLoading(false);
+    }
+  }
+
   useEffect(() => {
-    (async function () {
-      try {
-        const response = await fetch("http://localhost:3000/api/message");
-        const messageData = await response.json();
-        setMessage(messageData.message);
-      } catch (err) {
-        console.error("Error Occured", err);
-      }
-    })();
+    fetchMessage();
   }, []);
+
   return (
     <>
       <h1>DevStreak Dashboard</h1>
-      <h3>{message}</h3>
+      <button onClick={fetchMessage}>Refresh Message</button>
+      <h3>{loading ? <span>Loading...</span> : message}</h3>
     </>
   );
 }
