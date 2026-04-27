adf826e070a06082c260f146f0fac2458bb61b2f
Anoop Pemmaraju <anuppemmaraju@outlook.com>
2026-04-27 21:45:13 +0530

    Added TaskList in Frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from "react";
 function App() {
   const [message, setMessage] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [tasks, setTasks] = useState([]);
+  const [inputValue, setInputValue] = useState("");
 
   async function fetchMessage() {
     try {
@@ -20,6 +22,21 @@ function App() {
     }
   }
 
+  function handleTaskChange(e) {
+    setInputValue(e.target.value);
+  }
+
+  function addTask() {
+    const trimmed = inputValue.trim();
+    if (!trimmed) return;
+
+    setTasks((prev) => {
+      return [...prev, trimmed];
+    });
+
+    setInputValue("");
+  }
+
   useEffect(() => {
     fetchMessage();
   }, []);
@@ -27,6 +44,21 @@ function App() {
   return (
     <>
       <h1>DevStreak Dashboard</h1>
+      <input
+        type="text"
+        id="task"
+        value={inputValue}
+        onChange={handleTaskChange}
+      />
+      <button onClick={addTask} disabled={!inputValue}>
+        Add Task
+      </button>
+      <h3>Task List </h3>
+      <ul>
+        {tasks.map((t, index) => (
+          <li key={index}>{t}</li>
+        ))}
+      </ul>
       <button onClick={fetchMessage}>Refresh Message</button>
       <h3>
         {loading ? (
